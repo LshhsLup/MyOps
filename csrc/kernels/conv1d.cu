919b04c3caf50a636edd426a3413477b0cd4f640
LshhsLup <1571374817@qq.com>
2026-05-21 21:46:20 +0800

    perf:optimize the perf of conv1d

--- csrc/kernels/conv1d.cu
+++ csrc/kernels/conv1d.cu
@@ -9,11 +9,11 @@
 namespace myops {
 
 template <typename scalar_t>
-__global__ void conv1dKernel(scalar_t *__restrict__ output,
-                             const scalar_t *__restrict__ input,
-                             const scalar_t *__restrict__ kernel,
-                             const int input_size,
-                             const int kernel_size) {
+__global__ void conv1dKernel_v1(scalar_t *__restrict__ output,
+                                const scalar_t *__restrict__ input,
+                                const scalar_t *__restrict__ kernel,
+                                const int input_size,
+                                const int kernel_size) {
   int tid = blockIdx.x * blockDim.x + threadIdx.x;
   int output_size = input_size - kernel_size + 1;
   float sum{0.f};
@@ -27,6 +27,37 @@ __global__ void conv1dKernel(scalar_t *__restrict__ output,
 }
 
 // todo: use shared_memory
+template <typename scalar_t>
+__global__ void conv1dKernel_v2(scalar_t *__restrict__ output,
+                                const scalar_t *__restrict__ input,
+                                const scalar_t *__restrict__ kernel,
+                                const int input_size,
+                                const int kernel_size) {
+  constexpr int tile = threads_per_block();
+  __shared__ scalar_t smem_kernel[tile];
+  int global_tid = blockIdx.x * blockDim.x + threadIdx.x;
+  int block_tid = threadIdx.x;
+  int output_size = input_size - kernel_size + 1;
+  float sum{0.f};
+  for (int i = 0; i < (kernel_size + tile - 1) / tile; ++i) {
+    if (i * tile + block_tid < kernel_size) {
+      smem_kernel[block_tid] = kernel[i * tile + block_tid];
+    } else {
+      smem_kernel[block_tid] = FloatConverter<scalar_t>::from_float(0.f);
+    }
+    __syncthreads();
+    // one thread work
+    for (int j = 0; j < tile; ++j) {
+      if (global_tid < output_size && i * tile + j < kernel_size) {
+        scalar_t in = input[global_tid + i * tile + j];
+        sum += FloatConverter<scalar_t>::to_float(in) * FloatConverter<scalar_t>::to_float(smem_kernel[j]);
+      }
+    }
+  }
+  if (global_tid < output_size) {
+    output[global_tid] = FloatConverter<scalar_t>::from_float(sum);
+  }
+}
 
 template <typename scalar_t>
 void launchConv1dKernelImpl(scalar_t *output,
@@ -38,7 +69,7 @@ void launchConv1dKernelImpl(scalar_t *output,
   constexpr int threads = threads_per_block();
   int output_size = input_size - kernel_size + 1;
   int blocks = (output_size + threads - 1) / threads;
-  conv1dKernel<scalar_t>
+  conv1dKernel_v1<scalar_t>
       <<<blocks, threads, 0, stream>>>(output, input, kernel, input_size, kernel_size);
   MYOPS_CUDA_KERNEL_LAUNCH_CHECK();
 }
